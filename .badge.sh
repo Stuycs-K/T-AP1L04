0804b01ab0450e7f31a4962aa15a3900db3cd247
konstantinnovation <konstans@stuy.edu>
2025-10-16 08:57:33 -0400

    Update .badge.sh

--- .badge.sh
+++ .badge.sh
@@ -29,24 +29,3 @@ if [[ "$repo_name" == $COURSE* ]]; then
 else
     echo "ℹ️ Repo $repo_name does not start with 'AP' → no badge added"
 fi
-
-
-# # Step 1: Add the top line if the repo contains "AP"
-# if head -n 1 "$readme_file" | grep -q "AP1L"; then
-#     tail -n +2 "$readme_file" > "${readme_file}.tmp"
-#     echo "$badge_line" | cat - "${readme_file}.tmp" > "$readme_file"
-#     rm -f "${readme_file}.tmp"
-#     echo "🧹 Replaced top line containing 'AP1L'"
-# fi
-
-# # Step 2: Add the badge if it's not already present
-# if ! grep -Fq "$badge_line" "$readme_file"; then
-#     echo -e "\n$badge_line" >> "$readme_file"
-#     echo "✅ Added CI badge for $repo_name"
-#     git config user.name "github-actions[bot]"
-#     git config user.email "github-actions[bot]@users.noreply.github.com"
-#     git add .
-#     git diff --cached --quiet || (git commit -m "Run update.sh on repo creation" && git push)
-# else
-#     echo "ℹ️  CI badge already present"
-# fi
